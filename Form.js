23dd8d5c451a716c69f7b028cd8a1d9d0154fafd
chopid-ram <lozanojhon309@gmail.com>
2024-10-18 16:47:33 -0500

    ajuste 12/10/2024

--- Form.js
+++ Form.js
@@ -1,34 +1,32 @@
-function selectedValue(){
+    let valorfinal = "";
+    
+    // Obtener el botón de envío por su ID
+    const boton = document.getElementById("payment-widget");
+    const orderNumber = 'order_' + Math.floor(Math.random() * 1000000000);
+    
+    function HandleChange(e){
+        const tex = document.getElementById("pw_ammount");
+      console.log(tex);
+     tex.textContent = e.target.value;
+    
+    }
+    
+    document.getElementById("plan_mensual").addEventListener("change", HandleChange);
+    document.getElementById("plan_trimestral").addEventListener("change", HandleChange);
+    document.getElementById("plan_semestral").addEventListener("change", HandleChange);
+    document.getElementById("plan_anual").addEventListener("change", HandleChange);
 
-  let valorfinal = "";
+    document.addEventListener('DOMContentLoaded', function() {
+        function HandleChange(e, input) {
+            // toma el input que quieres que se llene automatico y cambia el valor
+            document.getElementById(input).value = e.target.value;
+        }
+        //aqui pones el input que llenas                           y aqui pones el que se llene automatico, id              
+        document.getElementById("nombre_completo").addEventListener('change', (e)=> HandleChange(e, "pw_manes"));
+        document.getElementById("nombre_completo").addEventListener('change', (e)=> HandleChange(e, "pw_lsatManes"));
+        document.getElementById("tipo_documento").addEventListener('change', (e)=> HandleChange(e, "pw_ytpeDco"));
+        document.getElementById("numero_documento").addEventListener('change', (e)=> HandleChange(e, "pw_mnuDco"));
+        document.getElementById("email").addEventListener('change', (e)=> HandleChange(e, "pw_email"));
+        });
 
-  const selectedValue = document.getElementById('pw_ammount').innerText;
-        valorfinal = selectedValue;
-  
-}
-
-function orderNumber(){
-
-  const orderNumber = `order_${Math.floor(Math.random() * 1000000000)}`;
-  console.log(orderNumber);
-  
-}
-
-function Apikey(){
-
-  const API_KEY = "N2Y5NGQyZWQ2NTljZmQyY2VjMGRhZjVkMTZjY2MxNmNkNzk1YmE5NWM0ZWM2OTg5YWUzMzQ5NDhmM2NjYzlkMDkyMTZhMWEzMTJkOGYyYWZjYjRhOWE3ZGUyODYwOWI5ZGQ0NmI3ZGRjZjVkOGI0NWMzNzgxOGEyZDgxZDEwNjY=";
-  console.log(API_KEY);
-
-}  
-
-function OcultarBtn(){
-
-const pwModalBoton = document.getElementById("pwModal");
-pwModalBoton.style.display = "none";
-
-  var pwPayModal = document.getElementById("pwPayModal");
-      pwPayModal.classList.remove("container-13");
-      pwPayModal.classList.add("modalContainer-pw", "container-13");
-
-}
-                       
+        
